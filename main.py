0d93e132c8b1c2607784239936dea680edeb27b9
10-abhi <abhishek81936@gmail.com>
2025-08-29 23:34:14 +0530

    shifted from tesseract to easyocr

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI , File , UploadFile
-import pytesseract
-from PIL import Image , ImageOps , ImageFilter
+import easyocr
 import io
+from PIL import Image
+import numpy as np
 
 app = FastAPI()
+
+#load model once (important for speed!)
+reader = easyocr.Reader(['en','hi'])
 
 @app.post("/ocr")
 async def extract_text(file: UploadFile = File(...)):
@@ -11,24 +15,18 @@ async def extract_text(file: UploadFile = File(...)):
         contents = await file.read()
         image = Image.open(io.BytesIO(contents))
     except Exception:
-        return {"error": "Invalid image file"}
-
-    #convert bytes to pil image
-    image = Image.open(io.BytesIO(contents))
-    #converting to grayscale
-    image = image.convert("L")
-    image = image.point(lambda x: 0 if x<140 else 255 ,'1')
-
-    #small font to hard 
-    new_size = (image.width * 2 , image.height * 2)
-    image = image.resize(new_size)
+        return {"error":"Invalid image file"}
     
-    #sharpen
-    image = image.filter(ImageFilter.SHARPEN)
-
-    #run ocr 
-    text = pytesseract.image_to_string(image, config="--oem 3 --psm 3").strip()
-    print("text", text);
-
-    return {"extracted_text" : text}
-
+    # Convert PIL image to numpy array
+    img_array = np.array(image)
+    result = reader.readtext(img_array)
+    
+    extracted_data = []
+    for bbox, text, conf in result:
+        status = "low_confidence" if conf < 0.5 else "high_confidence"
+        extracted_data.append({
+            "text": text,
+            "status": status
+        })
+    print(extracted_data)
+    return {"results": extracted_data}
