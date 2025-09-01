5cdbb5ab179d0ac254059aeac9a1af71bf3aac6b
10-abhi <abhishek81936@gmail.com>
2025-09-01 23:25:32 +0530

    integrated gemini

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import os
 from fastapi import FastAPI , File , UploadFile
 import easyocr
 import io
 from PIL import Image
 import numpy as np
+import google.generativeai as genai
+from dotenv import load_dotenv
+import logging
+
+load_dotenv()
+api_key = os.getenv("gemini_api_key")
 
 app = FastAPI()
 
 #load model once (important for speed!)
-reader = easyocr.Reader(['en','hi'])
+reader = easyocr.Reader(['en'])
 
+#gemini pro
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel("models/gemini-1.5-flash")
+print("📌 Available models:", genai.list_models())
 @app.post("/ocr")
 async def extract_text(file: UploadFile = File(...)):
     try:
@@ -19,14 +30,38 @@ async def extract_text(file: UploadFile = File(...)):
     
     # Convert PIL image to numpy array
     img_array = np.array(image)
+
+    #run ocr
     result = reader.readtext(img_array)
+    raw_text = " ".join([res[1] for res in result if res[2]>0.5])
+
+    #prompting gemini to structure data
+    print("Available models:", genai.list_models())
+
+    prompt = f"""
+    You are a financial assistant. I extracted this receipt text:
+    ---
+    {raw_text}
+    ---
+    Return structured JSON with these fields:
+    - merchant
+    - date
+    - items (list of {{"name":..., "price":...}})
+    - currency
+    Only return valid JSON, no explanations.
+    """
+    try:
+        response = model.generate_content(prompt)
+        structured = getattr(response, "text", str(response))
+    except Exception as e:
+        return {"error": f"Gemini API failed: {str(e)}"}
     
-    extracted_data = []
-    for bbox, text, conf in result:
-        status = "low_confidence" if conf < 0.5 else "high_confidence"
-        extracted_data.append({
-            "text": text,
-            "status": status
-        })
-    print(extracted_data)
-    return {"results": extracted_data}
+    return {
+        "raw_text": raw_text,
+        "structured": structured
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
