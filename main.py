b46713d334b7cbe86fbe652da662c7b1fd4c02f3
10-abhi <abhishek81936@gmail.com>
2025-10-07 23:44:23 +0530

    enhanced main func

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI , File , UploadFile
 import easyocr
 import io
-from PIL import Image
+from PIL import Image , ImageEnhance , ImageFilter
 import numpy as np
 import joblib
-from dotenv import load_dotenv
+from extract_amount import extract_amount_and_date
 
-load_dotenv()
 app = FastAPI()
 
 # import google.generativeai as genai
@@ -33,12 +32,24 @@ async def extract_text(file: UploadFile = File(...)):
     except Exception:
         return {"error":"Invalid image file"}
     
-    # Convert PIL image to numpy array
+    #some image preprocessing
+    image = image.convert("L")                   
+    image = image.resize((image.width*3, image.height*3))
+    image = image.filter(ImageFilter.SHARPEN)
+    enhancer = ImageEnhance.Contrast(image)
+    image = enhancer.enhance(3)
+
+
+    # Convert image to numpy array
     img_array = np.array(image)
 
     #run ocr
     result = reader.readtext(img_array)
-    raw_text = clean_text(" ".join([res[1] for res in result if res[2]>0.5]))
+    print("text before cleaning :")
+    for bbox, text, conf in result:
+        print(text, conf)
+    print("text after cleaning :")
+    raw_text = clean_text(" ".join([res[1] for res in result if res[2]>0.3]))
 
     #prompting gemini to structure data
     # print("Available models:", genai.list_models())
@@ -66,11 +77,17 @@ async def extract_text(file: UploadFile = File(...)):
     #     "structured": structured
     # }
     predicted_category = pipeline.predict([raw_text])[0];
+    extracted = extract_amount_and_date(raw_text)
+
     print("raw_text" , raw_text);
     print( "predicted_category", predicted_category)
+    print("total_amount",extracted["total_amount"])
+    print("ext date" , extracted["date"])
     return {
-        "raw_text": raw_text,
-        "predicted_category": predicted_category
+        # "raw_text": raw_text,
+        "total_amount":extracted["total_amount"],
+        "predicted_category": predicted_category,
+        "date":extracted["date"],
     }
 
 
