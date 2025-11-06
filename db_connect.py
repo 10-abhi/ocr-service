a2ab3aa00e36724a7fce0f552448d74f439c3724
10-abhi <abhishek81936@gmail.com>
2025-11-06 14:20:23 +0530

    added db to use it for dynamic data for model training

---
+++ db_connect.py
@@ -0,0 +1,9 @@
+# db_connection
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
